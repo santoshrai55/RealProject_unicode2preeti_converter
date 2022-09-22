d2e5d4e898f72885adf3bff8e526db333a11c81c
Santosh Rai <rai.santosh55@gmail.com>
2022-09-22 22:29:44 +0545

    started the project

---
+++ unicode_to_preeti_converter/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for unicode_to_preeti_converter project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.0/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'unicode_to_preeti_converter.settings')
+
+application = get_asgi_application()
